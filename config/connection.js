4b280b51812cc267d9f8474050bd488d9b0b5972
Vincent McGargill <vincentmcgargill@gmail.com>
2020-09-12 16:36:59 -0600

    Moved connection into the connection directory

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,11 @@
+// Heroku Environmental Variables
+const username = process.env.username;
+const password = process.env.password;
+const database = process.env.database;
+const host = process.env.host;
+const mysql = require("mysql");
+
+// MySQL localhost connection for testing purposes only
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -5,5 +13,14 @@ const connection = mysql.createConnection({
     password: "password",
     database: "burgers_db"
 });
+
+// MySQL Connection for Heroku deployment
+// const connection = mysql.createConnection({
+//     host: host,
+//     port: 3306,
+//     user: username,
+//     password: password,
+//     database: database
+// });
 
 module.exports = connection;
