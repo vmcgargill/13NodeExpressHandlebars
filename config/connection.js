09eedfc66f73385679746a6f549d67a7cbbebdc3
Vincent McGargill <vincentmcgargill@gmail.com>
2020-09-12 22:27:58 -0600

    Deploying the add burger feature to Heroku

--- config/connection.js
+++ config/connection.js
@@ -6,22 +6,22 @@ const host = process.env.host;
 const mysql = require("mysql");
 
 // MySQL Connection for Heroku deployment
-// const connection = mysql.createConnection({
-//     host: host,
-//     port: 3306,
-//     user: username,
-//     password: password,
-//     database: database
-// });
+const connection = mysql.createConnection({
+    host: host,
+    port: 3306,
+    user: username,
+    password: password,
+    database: database
+});
 
 // MySQL localhost connection for testing purposes only
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
