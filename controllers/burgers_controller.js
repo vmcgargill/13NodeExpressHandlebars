1661d3eec02263fc9d9ba24ec525e8300e4a3d84
Vincent McGargill <vincentmcgargill@gmail.com>
2020-09-12 22:54:08 -0600

    HUGE MILESTONE: Added the devour burger and delete burger features. This app is now 100% complete and ready to turn in. All that is left is writing the readme.md file. Deploying the app to Heroku now

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,23 +16,20 @@ router.get("/", function(req, res) {
 
 router.post("/api/:burger", function(req, res) {
     burger.create([req.params.burger], function(result) {
-        burger.all(function(data) {
-            const createdBurgers = data.filter(burger => burger.devoured === 0);
-            const devouredBurgers = data.filter(burger => burger.devoured === 1);
-            res.render("index", {
-                created: createdBurgers,
-                devoured: devouredBurgers
-            });
-        })
+        res.json({ id: result.insertId })
     })
 });
 
-router.put("/api/:id", function() {
-    
+router.put("/api/:id", function(req, res) {
+    burger.update([req.params.id], function(result) {
+        res.json({ id: result.insertId })
+    })
 });
 
-router.delete("/api/:id", function() {
-    
+router.delete("/api/:id", function(req, res) {
+    burger.delete([req.params.id], function(result) {
+        res.json({ id: result.insertId })
+    })
 });
 
 module.exports = router;
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,7 @@
 const submitBurger = $('#submitBurger');
 const BurgerInput = $('#BurgerInput');
+const devourBTN = $('.devourBTN');
+const deleteBTN = $('.deleteBTN')
 
 const GetAllBurgers = () => {
     return $.ajax({
@@ -15,11 +17,43 @@ const CreateBurger = (burger) => {
     });
 };
 
+const DevourBurger = (id) => {
+    return $.ajax({
+      url: "/api/" + id,
+      method: "PUT",
+    });
+};
+
+const DeleteBurger = (id) => {
+    return $.ajax({
+      url: "/api/" + id,
+      method: "DELETE",
+    });
+};
+
 submitBurger.on('click', function(event) {
     event.preventDefault();
     let burger = BurgerInput.val();
     console.log(burger)
     CreateBurger(burger);
+    GetAllBurgers().then(function() {
+        location.reload();
+    });
+});
+
+devourBTN.on('click', function(event) {
+    event.preventDefault();
+    let burgerId = $(this).val();
+    DevourBurger(burgerId);
+    GetAllBurgers().then(function() {
+        location.reload();
+    });
+})
+
+deleteBTN.on('click', function(event) {
+    event.preventDefault();
+    let burgerId = $(this).val();
+    DeleteBurger(burgerId);
     GetAllBurgers().then(function() {
         location.reload();
     });
