a0d5294ec57efd07db0f7ccdef6235bb84eb2090
Vincent McGargill <vincentmcgargill@gmail.com>
2020-09-13 11:24:38 -0600

    Fixed button hover for the delete button. Removed the localhost connection.

--- config/connection.js
+++ config/connection.js
@@ -14,15 +14,6 @@ const connection = mysql.createConnection({
     database: database
 });
 
-// MySQL localhost connection for testing purposes only
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     database: "burgers_db"
-// });
-
 // Make connection.
 connection.connect(function(err) {
     if (err) throw err;
